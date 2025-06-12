bb88876ff230603c0918a0ebf13a187d95ba05d0
Deepu-19 <komminenijnanadeep17@gmail.com>
2025-06-12 14:58:48 +0530

    Day 4: Loop + condition tasks - even or odd digits count

--- Day4/evenorodd_digits.py
+++ Day4/evenorodd_digits.py
@@ -1,12 +1,16 @@
 n=int(input("Enter a number:"))
 even=0
 odd=0
+even_digits=[]
+odd_digits=[]
 while n>0:
     digit=n%10
     if digit%2==0:
+        even_digits.append(digit)
         even+=1
     else:
+        odd_digits.append(digit)
         odd+=1
     n=n//10
-print("Even Digits: ",even)
-print("Odd numbers: ",odd)
+print("Even Digits: ",even,", Even digits: ",even_digits)
+print("Odd numbers: ",odd,", Odd digits: ",odd_digits)
